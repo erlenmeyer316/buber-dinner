17253b51516df9ccc4e4da5afb3ce47920ae055a
Adam Dickison <adam.dickison@posteo.net>
2023-10-28 10:23:18 -0400

    namespace fix

--- BuberDinner.Contracts/Authentication/AuthenticationResponse.cs
+++ BuberDinner.Contracts/Authentication/AuthenticationResponse.cs
@@ -1,8 +1,10 @@
-namespace BuberDinner.Contracts.Authentication;
-public record AuthenticationResponse(
-    Guid Id,
-    string FirstName,
-    string LastName,
-    string Email,
-    string Token
-);
+namespace BuberDinner.Contracts.Authentication
+{
+    public record AuthenticationResponse(
+        Guid Id,
+        string FirstName,
+        string LastName,
+        string Email,
+        string Token
+    );
+}
