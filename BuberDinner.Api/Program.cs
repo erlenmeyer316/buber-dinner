ba914af5f1032709680487eeda758afc4057a800
Adam M. Dickison <dickison.12@osu.edu>
2025-12-09 14:49:55 -0500

    Part 1 - Project Setup

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,12 +1,17 @@
+using BuberDinner.Application;
+using BuberDinner.Application.Services.Authentication;
+using BuberDinner.Infastructure;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
---
+++ BuberDinner.Infrastructure/DependecyInjection.cs
@@ -0,0 +1,13 @@
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuberDinner.Infastructure;
+
+public static class DependecyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
