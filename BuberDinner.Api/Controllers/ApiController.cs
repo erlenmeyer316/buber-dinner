c569b4e33be6444165048fa98775199056d0941e
Adam M. Dickison <dickison.12@osu.edu>
2025-12-12 16:20:44 -0500

    Part 8 - Validation Behavior - MediatR + FluentValidation

--- BuberDinner.Api/Controllers/ApiController.cs
+++ BuberDinner.Api/Controllers/ApiController.cs
@@ -3,6 +3,7 @@ using ErrorOr;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BuberDinner.Api.Controllers
 {
@@ -11,8 +12,23 @@ namespace BuberDinner.Api.Controllers
     {        
         protected IActionResult Problem(List<Error> errors)
         {
+            if (errors.Count is 0)
+            {
+               return Problem();
+            }
+
+            if (errors.All(error => error.Type == ErrorType.Validation))
+            {
+                return ValidationProblem(errors);
+            }
+
             HttpContext.Items[HttpContextItemKeys.Errors] = errors;
             var firstError = errors[0];
+            return Problem(firstError);
+        }
+
+        private IActionResult Problem(Error firstError)
+        {
             var statusCode = firstError.Type switch
             {
                 ErrorType.Conflict => StatusCodes.Status409Conflict,
@@ -21,6 +37,19 @@ namespace BuberDinner.Api.Controllers
                 _ => StatusCodes.Status500InternalServerError,
             };
             return Problem(statusCode: statusCode, title: firstError.Description);
+        }
+
+        private IActionResult ValidationProblem(List<Error> errors)
+        {
+            var modelStateDictionary = new ModelStateDictionary();
+
+            foreach (var error in errors)
+            {
+                modelStateDictionary.AddModelError(
+                    error.Code,
+                    error.Description);
+            }
+            return ValidationProblem(modelStateDictionary);
         }
     }
 }
---
+++ BuberDinner.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,48 @@
+using System;
+using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using ErrorOr;
+using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+
+namespace BuberDinner.Application.Common.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse> : 
+    IPipelineBehavior<TRequest, TResponse>
+    where TRequest  : IRequest<TResponse>
+    where TResponse : IErrorOr
+{
+    private readonly IValidator<TRequest>? _validator = null;
+
+    public ValidationBehavior(IValidator<TRequest>? validator)
+    {
+        _validator = validator;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (_validator is null)
+        {
+            return await next();
+        }
+
+        var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+
+        if (validationResult.IsValid)
+        {
+            return await next();
+        }
+
+        var errors = validationResult.Errors
+            .Select(ValidationFailure => Error.Validation(
+                ValidationFailure.PropertyName,
+                ValidationFailure.ErrorMessage))
+            .ToList();;
+        
+        return (dynamic)errors;
+    }
+}
