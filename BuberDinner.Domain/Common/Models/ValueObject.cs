8a0f71b75c99e2e4e94c2496ab369d5207b988c8
Adam M. Dickison <dickison.12@osu.edu>
2025-12-15 12:57:26 -0500

    Part 12 - Implementing AggregateRoot, Entity, & ValueObject

---
+++ BuberDinner.Domain/Common/Models/ValueObject.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BuberDinner.Domain.Common.Models;
+
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    public abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        var valueOject = (ValueObject)obj;
+
+        return GetEqualityComponents()
+            .SequenceEqual(valueOject.GetEqualityComponents());
+    }
+
+    public static bool operator ==(ValueObject left, ValueObject right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(ValueObject left, ValueObject right)
+    {
+        return !Equals(left, right);
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Select(x => x?.GetHashCode() ?? 0) // if null return 0
+            .Aggregate((x, y) => x ^ y); // bitwise compare (xor)
+}
+
+    public bool Equals(ValueObject? other)
+    {
+        return Equals((object?)other);
+    }
+}
